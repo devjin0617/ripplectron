020f1af76bf818bc1e9d77c0f2b5fcd5285ddea1
devjin0617 <devjin0617@gmail.com>
2017-07-29 22:54:22 +0900

    Ready to i18n

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,6 +5,8 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import VueI18n from 'vue-i18n'
+
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 
@@ -14,6 +16,7 @@ const api = new RippleAPI({ server: 'wss://s1.ripple.com:443' })
 
 Vue.prototype.$ripple = api
 
+Vue.use(VueI18n)
 Vue.use(ElementUI)
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
